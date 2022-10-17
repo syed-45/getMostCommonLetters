d4bcbab08ff2a557487a0eab3b3555f13ec20318
syed <user.email>
2022-10-17 11:37:25 +0100

    s

--- src/getMostCommonLetters.test.ts
+++ src/getMostCommonLetters.test.ts
@@ -6,4 +6,14 @@ test("finds top N occuring letters in given phrase", () => {
     [ 'e', 2 ], 
     [ 'o', 2 ]
   ]);
+  expect(getMostCommonLetters("This is a sentence",4)).toEqual([ 
+    [ 'e', 3 ], 
+    [ 's', 3 ],
+    [ 'i', 2 ],
+    [ 'n', 2 ]
+  ]);
+  expect(getMostCommonLetters("eeeeeeo",4)).toEqual([ 
+    [ 'e', 6 ], 
+    [ 'o', 1 ],
+  ]);
 });
